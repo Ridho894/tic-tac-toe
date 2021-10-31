334aa11fefea837dc0cd21eb62db1e8e03717949
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2021-10-31 10:36:40 +0700

    game complete build

--- containers/Board.tsx
+++ containers/Board.tsx
@@ -1,17 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Square from "../components/Square";
+
+type Player = "X" | "O" | "BOTH" | null;
+
+function calculateWinner(squares: Player[]) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
 function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [currentPlayer, setCurrentPlayer] = useState<"X" | "O">(
     Math.round(Math.random() * 1) === 1 ? "X" : "O"
   );
-  const [winner, setWinner] = useState(null);
+  const [winner, setWinner] = useState<Player>(null);
   function reset() {
     setSquares(Array(9).fill(null));
     setWinner(null);
     setCurrentPlayer(Math.round(Math.random() * 1) === 1 ? "X" : "O");
   }
-  function setSquareValue(index: any) {
+  function setSquareValue(index: number) {
     const newData = squares.map((val, i) => {
       if (i === index) {
         return currentPlayer;
@@ -21,9 +43,26 @@ function Board() {
     setSquares(newData);
     setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
   }
+
+  useEffect(() => {
+    const w = calculateWinner(squares);
+    if (w) {
+      setWinner(w);
+    }
+    if (!w && !squares.filter((square) => !square).length) {
+      setWinner("BOTH");
+    }
+  });
+
   return (
     <div>
-      <p>Hey {currentPlayer}, it's your turn</p>
+      {!winner && <p>Hey {currentPlayer}, it's your turn</p>}
+      {winner && winner !== "BOTH" && (
+        <p>Congratulations {winner} you win!!!</p>
+      )}
+      {winner && winner === "BOTH" && (
+        <p>Congratulations you're both winners</p>
+      )}
       <div className="grid">
         {Array(9)
           .fill(null)
