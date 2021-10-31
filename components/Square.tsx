9e522da7bb4ff05e0d49b48cfc9053fb69b26d13
Muhamad Ridho Budikusuma <77140390+Ridho894@users.noreply.github.com>
2021-10-31 10:20:19 +0700

    styling board

---
+++ components/Square.tsx
@@ -0,0 +1,29 @@
+type Player = "X" | "O" | null;
+function Square({
+  value,
+  onClick,
+  winner,
+}: {
+  winner: Player;
+  value: Player;
+  onClick: () => void;
+}) {
+  if (!value) {
+    return (
+      <button
+        className={"square"}
+        onClick={onClick}
+        disabled={Boolean(winner)}
+      />
+    );
+  }
+  return (
+    <div>
+      <button disabled className={`square square_${value.toLocaleLowerCase()}`}>
+        {value}
+      </button>
+    </div>
+  );
+}
+
+export default Square;
